f47760ef0edc256778421aa3224a1432ccb4bf02
Jeshnil Kumar <kumar_796@hotmail.com>
2026-02-15 23:48:54 +1100

    Added ability for app to delete a fruit from list, causes a re-render

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const products = ["Oranges", "Apples", "Kiwifruit", "Grapes", "Strawberries"];
+  const [products, setProducts] = useState([
+    "Oranges",
+    "Apples",
+    "Kiwifruit",
+    "Grapes",
+    "Strawberries",
+  ]);
   const [productCount, setProductCount] = useState(
     new Array(products.length).fill(0),
   );
@@ -18,14 +24,24 @@ function App() {
     arr[index] = arr[index] - 1;
     setProductCount([...arr]);
   }
+
+  function handleDelete(index) {
+    const arr = [...products];
+    arr.splice(index, 1);
+    setProducts([...arr]);
+    console.log(products);
+  }
+
   return (
     <>
+      <button>Add a fruit</button>
       <ul>
         {products.map((product, index) => (
           <li key={product}>
             <span>{product}</span> - <span>Votes: {productCount[index]}</span>
             <button onClick={() => handlePlus(index)}>+</button>
             <button onClick={() => handleMinus(index)}>-</button>
+            <button onClick={() => handleDelete(index)}>Delete</button>
           </li>
         ))}
       </ul>
